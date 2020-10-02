2f5c76ae3fc5cbec574609d1dfe04d45ed40e174
serii <seriiburduja@gmail.com>
2020-10-02 16:27:02 +0300

    cycles modal windows

--- src/js/main.js
+++ src/js/main.js
@@ -7,5 +7,6 @@ window.addEventListener('DOMContentLoaded', function (){
 	modals();
 	tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
 	tabs('.decoration_slider', '.decoration_item', '.decoration_content > div > div', 'slick-current');
+	tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more');
 	postForms();
 });
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,11 +1,19 @@
 export default function modals() {
-	function bindModal(openModalSelector, modalSelector, closeModalSelector) {
+	function bindModal(openModalSelector, modalSelector, closeModalSelector, closeModalOnOverlay = true) {
 		const openModal = document.querySelectorAll(openModalSelector);
 		const modal = document.querySelector(modalSelector);
 		const closeModal = document.querySelector(closeModalSelector);
+		const allModals = document.querySelectorAll('[data-modal]');
+
+		function closeAllModals(){
+			allModals.forEach(item => {
+				item.style.display = 'none';
+			});
+		}
 
 		openModal.forEach(item => {
 			item.addEventListener('click', (e) => {
+				closeAllModals();
 				if (e.target) {
 					e.preventDefault();
 				}
@@ -15,12 +23,14 @@ export default function modals() {
 		});
 
 		closeModal.addEventListener('click', () => {
+			closeAllModals();
 			modal.style.display = 'none';
 			document.body.style.overflow = '';
 		});
 
 		modal.addEventListener('click', (e) => {
-			if (e.target === modal) {
+			if (e.target === modal && closeModalOnOverlay) {
+				closeAllModals();
 				modal.style.display = 'none';
 				document.body.style.overflow = '';
 			}
@@ -35,7 +45,10 @@ export default function modals() {
 		}, time);
 	}
 
-	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close', false);
 	bindModal('.phone_link', '.popup', '.popup .popup_close');
+	bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+	bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+	bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
 	// openModalByTime('.popup', 3000);
 };
