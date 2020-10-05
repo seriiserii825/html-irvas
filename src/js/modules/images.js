1d79108ea2d931647043edb75a6696bf87253988
serii <seriiburduja@gmail.com>
2020-10-05 22:11:15 +0300

    first part

--- src/js/modules/images.js
+++ src/js/modules/images.js
@@ -1,8 +1,10 @@
+import calcScroll from "./calc-scroll";
 export default function gallery(){
 	const popup = document.querySelector('.popup-gallery');
 	const imageBlock = document.querySelector('.image-block');
 	const image = imageBlock.querySelector('img');
 	const gallery = document.querySelector('#js-gallery');
+	const scrollWidth = calcScroll();
 
 	gallery.addEventListener('click', function (event){
 		event.preventDefault();
@@ -12,11 +14,15 @@ export default function gallery(){
 			const imgPath = target.closest('a').getAttribute('href');
 			image.setAttribute('src', imgPath);
 			popup.style.display = 'flex';
+			image.classList.add('scale');
 			document.body.style.overflow = 'hidden';
+			document.body.style.marginRight = `${scrollWidth}px`;
 		}
-		if(target.matches('div.popup')){
-			div.style.display = 'none';
+		if(target.matches('div.popup-gallery')){
+			popup.style.display = 'none';
+			image.classList.remove('scale');
 			document.body.style.overflow = '';
+			document.body.style.marginRight = `0`;
 		}
 	});
 };
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,8 +1,10 @@
+import calcScroll from "./calc-scroll";
 export default function modals() {
 	function bindModal(openModalSelector, modalSelector, closeModalSelector) {
 		const openModal = document.querySelectorAll(openModalSelector);
 		const modal = document.querySelector(modalSelector);
 		const closeModal = document.querySelector(closeModalSelector);
+		const scrollWidth = calcScroll();
 
 		openModal.forEach(item => {
 			item.addEventListener('click', (e) => {
@@ -11,18 +13,21 @@ export default function modals() {
 				}
 				modal.style.display = 'block';
 				document.body.style.overflow = 'hidden';
+				document.body.style.marginRight = `${scrollWidth}px`;
 			});
 		});
 
 		closeModal.addEventListener('click', () => {
 			modal.style.display = 'none';
 			document.body.style.overflow = '';
+			document.body.style.marginRight = `0`;
 		});
 
 		modal.addEventListener('click', (e) => {
 			if (e.target === modal) {
 				modal.style.display = 'none';
 				document.body.style.overflow = '';
+				document.body.style.marginRight = `0`;
 			}
 		});
 	}
